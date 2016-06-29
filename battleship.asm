92839fd4e81c2070291f5679c94006850ab529aa
raymonddavis <raymond.davis.4th@gmail.com>
2016-06-29 18:35:45 -0500

    Ships Plotting Randomly (Still off grid sometimes) Fixes #1

--- battleship.asm
+++ battleship.asm
@@ -9,6 +9,13 @@ system_shot:	.asciiz "\nSystem Turn:"
 duplicate_shot:	.asciiz	"\n***** A duplicate shot! *****"
 invalid_shot:	.asciiz	"\n***** An invalid shot!  *****"
 next_game:	.asciiz "\nNew game? (y/n) "
+test:		.asciiz "test"
+
+cruiser_hori:	.word 4, 5, 10, 11, 16, 17, 22, 23, 28, 29, 34, 35
+cruiser_vert:	.word 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+
+destroyer_hori:	.word 5, 11, 17, 13, 29, 35
+destroyer_vert:	.word 30, 31, 32, 33, 34, 35
 
 user:   .ascii   "\n\n    . . . . . .     . . . . . .      0 1 2 3 4 5"
          .ascii     "\n    . . . . . .     . . . . . .      6 7 8 9 a b"
@@ -50,211 +57,428 @@ buf:	.space	200
 .text 
 .globl main
 
-main:					# program starts here
-	la	$a0, start_message	# load start_message message
-	li	$v0, 4			# print string
-	syscall				# print
-	la	$a0, user		# load user
-	li	$v0, 4 			# print string
-	syscall				# print
-	la	$a0, cruiser		# load cruiser message
-	li	$v0, 4 			# print string
-	syscall				# print
-	la	$a0, buf		# load buf
-	li	$a1, 4			# 4 bytes
-	li	$v0, 8			# read string
-	syscall				# read
-	lb	$a0, buf		# first cruiser placement 
-	jal 	convert			# call convert
-	move	$a2, $a0		# move $a0 into $a2
-	lb	$a0, buf+1		# scond cruiser palcement
-	jal 	convert			# call convert
-	move	$a1, $a0		# move $a0 into $a1
-	lb	$a0, buf+2		# third cruiser placement	
-	jal 	convert			# call convert
-	move	$t0, $a0		# move $a0 into $t0
-	jal 	plot_ship_user		# call plot_ship
-	move	$t0, $a1		# move $a1 into $t0
-	jal 	plot_ship_user		# call plot_ship
-	move	$t0, $a2		# move $a2 into $t0
-	jal	plot_ship_user		# call plot_ship
-	la	$a0, user		# load user
-	li	$v0, 4 			# print string		
-	syscall				# print
-	la	$a0, destroyer		# load destroyer message
-	li	$v0, 4 			# print string
-	syscall				# print
-	la	$a0, buf		# load buf
-	li	$a1, 3			# 3 bytes
-	li	$v0, 8			# read string
-	syscall				# read
-	lb	$a0, buf		# first destroyer placement 
-	jal 	convert			# call convert
-	move	$a1, $a0		# move $a0 into $a1
-	lb	$a0, buf+1		# scond destroyer palcement
-	jal 	convert			# call convert
-	move	$t0, $a0		# move $a0 into $t0
-	jal 	plot_ship_user		# call plot_ship
-	move	$t0, $a1		# move $a1 into $t0
-	jal	plot_ship_user		# call plot_ship
-	la	$a0, user		# load user
-	li	$v0, 4 			# print string		
-	syscall				# print
-	la	$a0, submarine		# load destroyer message
-	li	$v0, 4			# print string
-	syscall				# print
-	la	$a0, buf		# load buf
-	li	$a1, 2			# 2 bytes
-	li	$v0, 8			# read string
-	syscall				# read
-	lb	$a0, buf		# submarine placement
-	jal 	convert			# call convert
-	move	$t0, $a0		# move $a0 into $t0
-	jal 	plot_ship_user		# call plot_ship
-	la	$a0, user		# load user
-	li	$v0, 4			# print string
-	syscall				# print
-	la	$t7, 0			# user score = 0 (once this hits 6 the game is over)
-	la	$t6, 0			# system score = 0 (once this hits 6 the game is over)
-	la	$t5, 0			# if hit this is 1 if miss this is 0
-	#Random place system ship
-	j next_shot_user			# jump to next_shot
-next_shot_user:				# prompts user for next shot
-	la	$a0, shot		# load shot message	
-	li	$v0, 4			# print string
-	syscall				# print
-	la	$a0, buf		# load buf
-	li	$a1, 2			# 2 bytes
-	li	$v0, 8			# read string
-	syscall				# read
-	lb	$a0, buf		# shot position
-	jal 	convert			# call convert
-	move	$t0, $a0		# move $a0 into $t0
-	jal 	plot_user		# call plot
-	la	$a0, user		# load user
-	li	$v0, 4			# print string
-	syscall				# print
-	beq	$t7, 6, new_game	# $t7 == 6 call new_game
-	beq	$t5, 0, next_shot_system
-	j 	next_shot_user		# call next_shot
-next_shot_system:
-	la	$a0, system_shot		# load shot message	
-	li	$v0, 4			# print string
+main:					
+	la	$a0, start_message	
+	li	$v0, 4			
+	syscall				
+	la	$a0, user		
+	li	$v0, 4 			
+	syscall				
+	la	$a0, cruiser		
+	li	$v0, 4 			
+	syscall				
+	la	$a0, buf		
+	li	$a1, 4			
+	li	$v0, 8			
+	syscall				
+	lb	$a0, buf		
+	jal 	convert			
+	move	$a2, $a0		
+	lb	$a0, buf+1		
+	jal 	convert			
+	move	$a1, $a0		
+	lb	$a0, buf+2			
+	jal 	convert			
+	move	$t0, $a0		
+	jal 	plot_ship_user		
+	move	$t0, $a1		
+	jal 	plot_ship_user		
+	move	$t0, $a2		
+	jal	plot_ship_user		
+	la	$a0, user		
+	li	$v0, 4 			
+	syscall				
+	la	$a0, destroyer		
+	li	$v0, 4 			
+	syscall				
+	la	$a0, buf		
+	li	$a1, 3			
+	li	$v0, 8			
+	syscall				
+	lb	$a0, buf		
+	jal 	convert			
+	move	$a1, $a0		
+	lb	$a0, buf+1		
+	jal 	convert			
+	move	$t0, $a0		
+	jal 	plot_ship_user		
+	move	$t0, $a1		
+	jal	plot_ship_user		
+	la	$a0, user		
+	li	$v0, 4 				
+	syscall				
+	la	$a0, submarine		
+	li	$v0, 4			
+	syscall				
+	la	$a0, buf		
+	li	$a1, 2			
+	li	$v0, 8			
+	syscall				
+	lb	$a0, buf		
+	jal 	convert			
+	move	$t0, $a0		
+	jal 	plot_ship_user		
+	la	$a0, user		
+	li	$v0, 4			
+	syscall				
+	la	$t7, 0			
+	la	$t6, 0			
+	la	$t5, 0			
+	jal 	system_cruiser
+	la	$a0, system		
+	li	$v0, 4			
 	syscall		
-	jal 	plot_system		# call plot
-	la	$a0, user		# load user
-	li	$v0, 4			# print string
-	syscall				# print
-	beq	$t6, 6, new_game	# $t7 == 6 call new_game
+	jal	system_destroyer
+	la	$a0, system		
+	li	$v0, 4			
+	syscall	
+	jal	system_submarine
+	la	$a0, system		
+	li	$v0, 4			
+	syscall
+	j	next_shot_user
+next_shot_user:				
+	la	$a0, shot			
+	li	$v0, 4			
+	syscall				
+	la	$a0, buf		
+	li	$a1, 2			
+	li	$v0, 8			
+	syscall				
+	lb	$a0, buf		
+	jal 	convert			
+	move	$t0, $a0		
+	jal 	plot_user		
+	la	$a0, user		
+	li	$v0, 4			
+	syscall				
+	beq	$t7, 6, new_game	
+	beq	$t5, 0, next_shot_system
+	j 	next_shot_user		
+next_shot_system:
+	la	$a0, system_shot		
+	li	$v0, 4			
+	syscall		
+	jal 	plot_system		
+	la	$a0, user		
+	li	$v0, 4			
+	syscall				
+	beq	$t6, 6, new_game	
 	beq	$t5, 0, next_shot_user
-	j 	next_shot_system	# call next_shot_system
-invalid:				# print invalid message and print user agian
-	la	$a0, invalid_shot	# load invalid message
-	li	$v0, 4			# print string
-	syscall				# print
-	la	$a0, user		# load user
-	li	$v0, 4			# print string
-	syscall				# print
-	j 	next_shot_user		# jump to next_shot
-new_game:				# prompts user if they want a new game
-	la	$a0, next_game		# load next_game message
-	li	$v0, 4			# print string
-	syscall				# print
-	la	$a0, buf		# load buf
-	li	$a1, 2			# 2 bytes
-	li	$v0, 8			# read string
-	syscall				# read
-	lb	$a0, buf		# load buf byte
-	beq 	$a0, 'n', exit		# $a0 == n call exit
-	li      $t0, 0			# set counter to 0
-L10:	lb      $t1, reset($t0)		# load byte from reset
-	sb      $t1, user($t0)		# load byte into user
-	sb	$t1, system($t0)	# load byte into system
-	add     $t0, $t0, 1		# increment $t0
-	beq     $t0, 280, L20		# $t0 == 280 jump tp L20
-	j       L10			# repeat loop
-L20:    j main				# jump to main
-exit:					# ends the game
-	li	$v0, 10			# print exit
-	syscall				# print
-convert: 				# convert input into correct int 0-9 or a-z (returns to line under called position)
-	sub	$a0, $a0, 48		# subtract 48 to get number value
-	blt	$a0, 0, under 		# > 0 goto under
-	bgt 	$a0, 9, over 		# > 9 goto over
-	jr 	$ra 			# jump to line under convert call
-under:					# if input is a symbol or other	(called from under returns to line under convert)
-	beq	$a0, -1, show_system	# if -1 show system (/)
+	j 	next_shot_system	
+invalid:				
+	la	$a0, invalid_shot	
+	li	$v0, 4			
+	syscall				
+	la	$a0, user		
+	li	$v0, 4			
+	syscall				
+	j 	next_shot_user		
+new_game:				
+	la	$a0, next_game		
+	li	$v0, 4			
+	syscall				
+	la	$a0, buf		
+	li	$a1, 2			
+	li	$v0, 8			
+	syscall				
+	lb	$a0, buf		
+	beq 	$a0, 'n', exit		
+	li      $t0, 0			
+L10:	lb      $t1, reset($t0)		
+	sb      $t1, user($t0)		
+	sb	$t1, system($t0)	
+	add     $t0, $t0, 1		
+	beq     $t0, 280, L20		
+	j       L10			
+L20:    j 	main				
+exit:					
+	li	$v0, 10			
+	syscall				
+convert: 				
+	sub	$a0, $a0, 48		
+	blt	$a0, 0, under 		
+	bgt 	$a0, 9, over 		
+	jr 	$ra 			
+under:					
+	beq	$a0, -1, show_system	
 	li	$a0, -5
-	beq	$a0, -5, invalid	# if invalid input call invalid
-	jr	$ra			# jump to line under convert call
-over:					# if the input is a character (called from convert return to line under convert)
+	beq	$a0, -5, invalid	
+	jr	$ra			
+over:					
 	blt	$a0, 49, invalid
-	sub	$a0, $a0, 39		# a-z
-	jr	$ra			# jump to line under convert call
+	sub	$a0, $a0, 39		
+	jr	$ra			
 show_system:
-	la	$a0, system		# load system
-	li	$v0, 4			# print string
-	syscall				# print
-	j	next_shot_user		# jump to next_shot_user
-plot_ship_user:				# plots ships (returns to line under plot)
-	mul 	$t0, $t0, 2		# mulitply $t0 by 2
-	li 	$t2, 'O'		# store ship marker (O) in $t2
-	lh 	$t1, offset1($t0)	# get offset for grid 1 store in $t1
-	sb 	$t2, user($t1)		# plot $t2 on user using offset $t1
-	jr	$ra			# jump to line under plot_ship call
-plot_user:				# plot the shot (returns to line under called position)
-	mul 	$t0, $t0, 2		# mulitply $t0 by 2
-	lh	$t1, offset1($t0)	# get offset for grid 1 store in $t1
-	lh	$t2, offset2($t0)	# get offset for grid 2 store in $t2
-	lb	$t0, system($t1)		# load byte $t0 from user using offset $t1
-	beq	$t0, 'O', plot_hit_user	# $t0 == 0 call plot_hit
-	beq	$t0, '.', plot_miss_user	# $t0 == . call plot_miss
-	la	$a0, duplicate_shot	# else load duplicate_shot message
-	li	$v0, 4			# print string
-	syscall				# print
-	jr 	$ra			# return to line under plot
-plot_hit_user:				# plot hits (called from plot returns to line under plot)
+	la	$a0, system		
+	li	$v0, 4			
+	syscall				
+	j	next_shot_user		
+plot_ship_user:				
+	mul 	$t0, $t0, 2		
+	li 	$t2, 'O'		
+	lh 	$t1, offset1($t0)	
+	sb 	$t2, user($t1)		
+	jr	$ra			
+plot_user:				
+	mul 	$t0, $t0, 2		
+	lh	$t1, offset1($t0)	
+	lh	$t2, offset2($t0)	
+	lb	$t0, system($t1)		
+	beq	$t0, 'O', plot_hit_user	
+	beq	$t0, '.', plot_miss_user	
+	la	$a0, duplicate_shot	
+	li	$v0, 4			
+	syscall				
+	jr 	$ra			
+plot_hit_user:				
 	li	$t5, 1
-	add	$t7, $t7, 1		# increment score (at 6 game over)
-	li 	$t3, 'X'		# store hit marker (X) in $t3
-	sb 	$t3, system($t1)		# plot $t3 on user using offset $t1
-	sb 	$t3, user($t2)		# plot $t3 on user using offset $t2
-	jr	$ra			# jump to line under plot_hit call
-plot_miss_user:				# plot misses (called from plot returns to line under plot)
+	add	$t7, $t7, 1		
+	li 	$t3, 'X'		
+	sb 	$t3, system($t1)		
+	sb 	$t3, user($t2)		
+	jr	$ra			
+plot_miss_user:				
 	li	$t5, 0
-	li 	$t3, '+'		# store hit marker (+) in $t3
-	sb 	$t3, system($t1)		# plot $t3 on user using offset $t1
-	sb 	$t3, user($t2)		# plot $t3 on user using offset $t2
-	jr	$ra			# jump to line under plot_miss call
-plot_ship_system:			# plots ships (returns to line under plot)
-	mul 	$t0, $t0, 2		# mulitply $t0 by 2
-	li 	$t2, 'O'		# store ship marker (O) in $t2
-	lh 	$t1, offset1($t0)	# get offset for grid 1 store in $t1
-	sb 	$t2, system($t1)	# plot $t2 on user using offset $t1
-	jr	$ra			# jump to line under plot_ship call
-plot_system:				# plot the shot (returns to line under called position)
+	li 	$t3, '+'		
+	sb 	$t3, system($t1)		
+	sb 	$t3, user($t2)		
+	jr	$ra			
+system_cruiser:
+	li	$a1, 2
+	li 	$v0, 42       
+	add 	$a0, $a0, 2
+	syscall
+	#li	$a0, 1
+	beq	$a0, 1, system_cruiser_hori
+	beq 	$a0, 0, system_cruiser_vert
+system_cruiser_hori:
+	la	$t2, cruiser_hori
+	j 	LSCH2
+LSCH1:	
+	beq	$t0, $t2, LSCH2
+	addi	$t2, $t2, 2
+	add     $t3, $t3, 1		
+	beq     $t3, 13, LSCH3		
+	j       LSCH1			
+LSCH2:
+	li	$t3, 0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	random_number
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+        la	$a3, ($a0)
+        move	$t0, $a3
+	j 	LSCH1
+LSCH3:
+	la	$a1, ($a0)
+	add	$a1, $a1, 1
+	la	$a2, ($a1)
+	add	$a2, $a2, 1
+	move	$t0, $a0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move	$t0, $a1
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move	$t0, $a2
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4	
+	jr	$ra
+system_cruiser_vert:
+	j 	LSCV2
+LSCV1:	lb      $t2, cruiser_vert($t3)		
+	beq	$t0, $t2, LSCV2
+	add     $t3, $t3, 1		
+	beq     $t3, 13, LSCV3		
+	j       LSCV1			
+LSCV2:
+	li	$t3, 0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	random_number
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+        move	$t0, $a0
+	j 	LSCV1
+LSCV3:
+	la	$a1, ($a0)
+	add	$a1, $a1, 6
+	la	$a2, ($a1)
+	add	$a2, $a2, 6
+	move	$t0, $a0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move	$t0, $a1
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move	$t0, $a2
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4	
+	jr	$ra
+system_destroyer:
+	li	$a1, 2
+	li 	$v0, 42       
+	add 	$a0, $a0, 2
+	syscall
+	li	$a0, 0
+	beq	$a0, 1, system_destroyer_hori
+	beq 	$a0, 0, system_destroyer_vert
+system_destroyer_hori:
+	j 	LSCH2
+LSDH1:	lb      $t2, destroyer_hori($t3)
+	la	$t4, ($t0)
+	mul 	$t4, $t4, 2		
+	lh	$t1, offset1($t4)		
+	lb	$t4, system($t1)		
+	beq	$t4, 'O', LSDV2			
+	beq	$t0, $t2, LSDH2
+	add     $t3, $t3, 1		
+	beq     $t3, 8, LSDH3		
+	j       LSDH1			
+LSDH2:
+	li	$t3, 0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	random_number
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+      	la	$t0, ($a0)
+	j 	LSDH1
+LSDH3:
+	la	$a1, ($a0)
+	add	$a1, $a1, 1
+	move	$t0, $a0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move	$t0, $a1
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4	
+	jr	$ra
+system_destroyer_vert:
+	j 	LSDV2
+LSDV1:	lb      $t2, destroyer_vert($t3)
+	la	$t4, ($t0)
+	mul 	$t4, $t4, 2		
+	lh	$t1, offset1($t4)		
+	lb	$t4, system($t1)		
+	beq	$t4, 'O', LSDV2		
+	beq	$t0, $t2, LSDV2
+	add     $t3, $t3, 1		
+	beq     $t3, 8, LSDV3		
+	j       LSDV1			
+LSDV2:
+	li	$t3, 0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	random_number
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+        la	$t0, ($a0)
+	j 	LSDV1
+LSDV3:
+	la	$a1, ($a0)
+	add	$a1, $a1, 6
+	move	$t0, $a0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move	$t0, $a1
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4	
+	jr	$ra
+system_submarine:
+	j 	LS2
+LS1:	
+	la	$t4, ($t0)
+	mul 	$t4, $t4, 2		
+	lh	$t1, offset1($t4)		
+	lb	$t4, system($t1)		
+	beq	$t4, 'O', LS2	
+	j       LS3			
+LS2:
+	li	$t3, 0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	random_number
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+      	la	$t0, ($a0)
+	j 	LS1
+LS3:
+	move	$t0, $a0
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	plot_ship_system
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	jr	$ra
+plot_ship_system:			
+	mul 	$t0, $t0, 2		
+	li 	$t2, 'O'		
+	lh 	$t1, offset1($t0)	
+	sb 	$t2, system($t1)	
+	jr	$ra			
+plot_system:				
+	subu  	$sp, $sp, 4      
+        sw    	$ra, ($sp) 
+	jal 	random_number		
+	lw    	$ra, ($sp)      
+        addu  	$sp, $sp, 4
+	move 	$t0, $a0
+	mul 	$t0, $t0, 2		
+	lh	$t1, offset1($t0)	
+	lh	$t2, offset2($t0)	
+	lb	$t0, user($t1)		
+	beq	$t0, 'O', plot_hit_system	
+	beq	$t0, '.', plot_miss_system	
+	j	plot_system
+plot_hit_system:			
+	li	$t5, 1
+	add	$t6, $t6, 1		
+	li 	$t3, 'X'		
+	sb 	$t3, user($t1)	
+	sb 	$t3, system($t2)	
+	jr	$ra			
+plot_miss_system:			
+	li	$t5, 0
+	li 	$t3, '+'		
+	sb 	$t3, user($t1)	
+	sb 	$t3, system($t2)	
+	jr	$ra			
+random_number:	
 	li	$a1, 35
 	li 	$v0, 42       
 	add 	$a0, $a0, 35
 	syscall
-	move	$t0, $a0		# move $a0 into $t0
-	mul 	$t0, $t0, 2		# mulitply $t0 by 2
-	lh	$t1, offset1($t0)	# get offset for grid 1 store in $t1
-	lh	$t2, offset2($t0)	# get offset for grid 2 store in $t2
-	lb	$t0, user($t1)		# load byte $t0 from user using offset $t1
-	beq	$t0, 'O', plot_hit_system	# $t0 == 0 call plot_hit
-	beq	$t0, '.', plot_miss_system	# $t0 == . call plot_miss
-	j	plot_system
-plot_hit_system:			# plot hits (called from plot returns to line under plot)
-	li	$t5, 1
-	add	$t6, $t6, 1		# increment score (at 6 game over)
-	li 	$t3, 'X'		# store hit marker (X) in $t3
-	sb 	$t3, user($t1)	# plot $t3 on user using offset $t1
-	sb 	$t3, system($t2)	# plot $t3 on user using offset $t2
-	jr	$ra			# jump to line under plot_hit call
-plot_miss_system:			# plot misses (called from plot returns to line under plot)
-	li	$t5, 0
-	li 	$t3, '+'		# store hit marker (+) in $t3
-	sb 	$t3, user($t1)	# plot $t3 on user using offset $t1
-	sb 	$t3, system($t2)	# plot $t3 on user using offset $t2
-	jr	$ra			# jump to line under plot_miss call
+	jr	$ra
